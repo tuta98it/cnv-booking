4333f43f50097f429e89033cc991f6d090ee4787
Trần Anh Tú <tutran1998.tt@gmail.com>
2023-10-27 15:45:30 +0700

    update: ui page data-airline-tickets
    Signed-off-by: Trần Anh Tú <tutran1998.tt@gmail.com>

--- src/app/component/portal/pages/data-airline-tickets/data-airline-tickets.component.ts
+++ src/app/component/portal/pages/data-airline-tickets/data-airline-tickets.component.ts
@@ -224,6 +224,8 @@ export class DataAirlineTicketsComponent extends TableSelectionAbstract implemen
           let stt = 0;
           this.datas.forEach((en: any) => {
             en.stt = ++stt;
+            en.totalNetPriceVATFormat = this.formatCurrencyVND(en.totalNetPrice * 0.08);
+            en.totalNetPriceFormat = this.formatCurrencyVND(en.totalNetPrice);
           });
           this.total = res.total;
           this.filteredDatas = this.datas;
@@ -1007,7 +1009,13 @@ export class DataAirlineTicketsComponent extends TableSelectionAbstract implemen
   }
 
   calculateSelectedRowPrice(options) {
-    if (options.name === 'SelectedRowsVAT') {
+    if (options.name === 'SelectedRowsSumPrice') {
+      if (options.summaryProcess === 'start') {
+        options.totalValue = 0;
+      } else if (options.summaryProcess === 'calculate') {
+        options.totalValue += options.value.totalNetPrice;
+      }
+    }else if (options.name === 'SelectedRowsVAT') {
       if (options.summaryProcess === 'start') {
         options.totalValue = 0;
       } else if (options.summaryProcess === 'calculate') {
