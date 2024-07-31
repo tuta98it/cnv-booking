fa04e676900548f81c934a1450b0c9af575f96c9
Trần Anh Tú <tutran1998.tt@gmail.com>
2024-07-31 17:08:43 +0700

    [feat] status request-booking
    Signed-off-by: Trần Anh Tú <tutran1998.tt@gmail.com>

--- src/app/enums/airline-ticket-booking-request-status.enum.ts
+++ src/app/enums/airline-ticket-booking-request-status.enum.ts
@@ -3,17 +3,17 @@ export enum AirlineTicketBookingRequestStatus {
   ReserveSeat = 2,
   ReceivedTicket = 3,
   ExpiredTicket = 4,
-  IssuedTicket = 5,
-  AdjustTicket = 6,
+  AdjustTicket = 5,
+  IssuedTicket = 6,
   All = 999,
 }
 
 export const AIRLINE_TICKET_BOOKING_REQUEST_STATUS_OPTIONS = [
   { label: '--Tất cả--', value: AirlineTicketBookingRequestStatus.All },
   { label: 'Gửi yêu cầu', value: AirlineTicketBookingRequestStatus.SubmitRequest },
-  { label: 'Đang giữ chỗ', value: AirlineTicketBookingRequestStatus.ReserveSeat },
-  { label: 'Đã xác nhận vé', value: AirlineTicketBookingRequestStatus.ReceivedTicket },
+  { label: 'Đang giữ vé', value: AirlineTicketBookingRequestStatus.ReserveSeat },
+  { label: 'Đã xác nhận giữ vé', value: AirlineTicketBookingRequestStatus.ReceivedTicket },
   { label: 'Hết hạn giữ vé', value: AirlineTicketBookingRequestStatus.ExpiredTicket },
-  { label: 'Xuất vé', value: AirlineTicketBookingRequestStatus.IssuedTicket },
   { label: 'Điều chỉnh vé', value: AirlineTicketBookingRequestStatus.AdjustTicket },
+  { label: 'Xuất vé', value: AirlineTicketBookingRequestStatus.IssuedTicket },
 ];
