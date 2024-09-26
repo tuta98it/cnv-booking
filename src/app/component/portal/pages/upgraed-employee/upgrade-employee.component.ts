5a44f453e0fc5615b90ed1ee299b8b4e1dabc256
Trần Anh Tú <tutran1998.tt@gmail.com>
2024-09-26 16:03:58 +0700

    changeValuePaymentPeriod PAYMENT_PERIOD_DAYS_OPTIONS
    Signed-off-by: Trần Anh Tú <tutran1998.tt@gmail.com>

--- src/app/component/portal/pages/upgraed-employee/upgrade-employee.component.ts
+++ src/app/component/portal/pages/upgraed-employee/upgrade-employee.component.ts
@@ -459,7 +459,8 @@ export class UpgradeEmployeeComponent implements OnInit {
         break;
 
       case PaymentPeriod.MONTH:
-        this.PAYMENT_PERIOD_DAYS_OPTIONS = DAYS_OF_MONTH_OPTIONS.filter((day: any) => day.value >= DaysOfMonth.ONE && day.value <= DaysOfMonth.TWENTY_EIGHT);
+        // this.PAYMENT_PERIOD_DAYS_OPTIONS = DAYS_OF_MONTH_OPTIONS.filter((day: any) => day.value >= DaysOfMonth.ONE && day.value <= DaysOfMonth.TWENTY_EIGHT);
+        this.PAYMENT_PERIOD_DAYS_OPTIONS = DAYS_OF_MONTH_OPTIONS;
         break;
 
       default:
