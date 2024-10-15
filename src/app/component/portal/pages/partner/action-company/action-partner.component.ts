c8c76e8988e38e2fc915703a9aa720b5b6f732ce
Trần Anh Tú <tutran1998.tt@gmail.com>
2024-10-15 11:10:11 +0700

    Optimize func handleChangeUploadEmployeeForPartnerFile
    Signed-off-by: Trần Anh Tú <tutran1998.tt@gmail.com>

--- src/app/component/portal/pages/partner/action-company/action-partner.component.ts
+++ src/app/component/portal/pages/partner/action-company/action-partner.component.ts
@@ -1002,22 +1002,22 @@ export class ActionPartnerComponent implements OnInit {
     if (info.file.status === 'done') {
       this.listUploadEmployeeForPartnerFile = info.fileList;
       setTimeout(() => {
+
+        if (info.file.response.isValid) {
+          info.file.response.errors.forEach((result: any) => {
+            const message = result?.errorMessage ?? "";
+            result.IsValid ? this.msg.success(message) : this.msg.error(message);
+          });
+
+        } else {
+          this.msg.error(`${info.file.response?.otherData ?? ""}`);
+        }
+
         // info.file.response.errors.forEach((error: any) => {
         //   this.msg.error(`${error?.errorMessage ?? ""}`);
         // });
-        if (info.file.response.isValid) {
-          info.file.response.errors.filter(e => e.IsValid == true).forEach((success: any) => {
-            this.msg.error(`${success?.errorMessage ?? ""}`);
-          });
-
-          info.file.response.errors.filter(e => e.IsValid == false).forEach((error: any) => {
-            this.msg.error(`${error?.errorMessage ?? ""}`);
-          });
-          this.msg.success(`${info.file.name} file tải lên thành công có thề tồn tại một vài nhân viên không đặt yêu cầu.`);
-        } else {
-          this.msg.error(`${info.file.name} file tải đã gặp lỗi hoặc tất các nhân viên không đạt yêu cầu.`);
-        }
-
+        // this.msg.success(`${info.file.name} file tải lên thành công có thề tồn tại một vài nhân viên không đặt yêu cầu.`);
+        // this.msg.error(`${info.file.name} file tải đã gặp lỗi hoặc tất các nhân viên không đạt yêu cầu.`);
         // if (this.listUploadEmployeeForPartnerFile.length > 0) {
         //   this.listUploadEmployeeForPartnerFile[this.listUploadEmployeeForPartnerFile.length - 1].uid = info.file.response.partnerFileId.toString();
         //   this.listUploadEmployeeForPartnerFile[this.listUploadEmployeeForPartnerFile.length - 1].url = `${this.configService.getConfig().api.baseUrl}/${info.file.response.path}`;
