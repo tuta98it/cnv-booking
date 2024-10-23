24a95db9b3ba2c953c453ed9ff55027e6cd785f9
Trần Anh Tú <tutran1998.tt@gmail.com>
2024-10-23 11:59:22 +0700

    [dev] MaskAsReadNotifications
    Signed-off-by: Trần Anh Tú <tutran1998.tt@gmail.com>

--- src/app/component/portal/pages/notifications/notifications.component.ts
+++ src/app/component/portal/pages/notifications/notifications.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { DxDataGridComponent } from 'devextreme-angular';
 import { ColorStatusNotifications, ColorTextViewDetailNotifications, NotificationVHLEnum } from 'src/app/enums/notification-vhl.enum';
 import { NotificationAPIService } from 'src/app/service/notification-service';
 import { NotificationService } from 'src/app/service/notification.service';
@@ -9,7 +10,11 @@ import { Constant } from 'src/app/shared/constants/constant.class';
   templateUrl: './notifications.component.html',
   styleUrls: ['./notifications.component.scss']
 })
-export class NotificationsComponent implements OnInit {
+export class NotificationsComponent implements OnInit, AfterViewInit{
+  @ViewChild('gridContainer', { static: false }) gridNotification: DxDataGridComponent;
+  allMode: string;
+  checkBoxesMode: string;
+
   NotificationVHLEnum = NotificationVHLEnum;
   ColorStatusNotifications = ColorStatusNotifications;
   ColorTextViewDetailNotifications = ColorTextViewDetailNotifications;
@@ -29,25 +34,45 @@ export class NotificationsComponent implements OnInit {
     private notificationAPIService: NotificationAPIService,
     private notificationService: NotificationService,
 
-  ) { }
+  ) {
+    this.allMode = 'page';
+    this.checkBoxesMode = 'always';
+  }
 
   ngOnInit(): void {
     this.handleGetListNotifications();
   }
 
+  ngAfterViewInit() {
+    // Now the grid reference will be available
+  }
 
 
   private handleGetListNotifications() {
     this.getListNotifications().then((result: any) => {
       this.listAllNotifications = result.data;
-      console.log(this.listAllNotifications);
       this.totalAllNotifications = result.total;
     }).catch((error: any) => {
 
-    })
+    });
   }
 
 
+  public handleMaskAsReadNotifications() {
+    const selectedRows = this.gridNotification.instance.getSelectedRowsData();
+    const selectedRowsUnreaded = selectedRows.filter(e => e.readed != true);
+    if(selectedRowsUnreaded == null || selectedRowsUnreaded == undefined || selectedRowsUnreaded?.length == 0){
+      // Đánh dấu những thông báo nào chưa đọc
+      return;
+    }
+    let notificationUnreadIds = selectedRowsUnreaded.map(e => e.id);
+    this.readedNotificationByIds(notificationUnreadIds).then(result => {
+      this.handleGetListNotifications();
+    }).catch(error => {
+
+    });
+
+  }
 
   private getListNotifications() {
     return new Promise((resolve, reject) => {
@@ -71,6 +96,34 @@ export class NotificationsComponent implements OnInit {
             //   }
             //   reject(res.errors);
             // }
+          },
+          error: (err: any) => {
+            reject(err);
+          },
+          complete: () => {
+          }
+        }
+      ).add(() => {
+      });
+    });
+  }
+
+  private readedNotificationByIds(idNotifications: number[]) {
+    return new Promise((resolve, reject) => {
+      this.notificationAPIService.readNotificationByIds(idNotifications).subscribe(
+        {
+          next: (res: any) => {
+            if (res.isValid) {
+              resolve(true);
+            } else {
+              if (res.errors && res.errors.length > 0) {
+                res.errors.forEach((el: any) => {
+                  this.notificationService.showNotification(Constant.ERROR, el.errorMessage);
+                });
+              } else {
+              }
+              reject(res.errors);
+            }
           },
           error: (err: any) => {
             reject(err);
