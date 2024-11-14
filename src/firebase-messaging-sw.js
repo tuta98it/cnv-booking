052527dabc99ce26f69dee51897ac3480fe84619
Trần Anh Tú <tutran1998.tt@gmail.com>
2024-11-14 18:04:32 +0700

    addEventListener push notifications
    Signed-off-by: Trần Anh Tú <tutran1998.tt@gmail.com>

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -1,3 +1,28 @@
+
+
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  // Link cần mở được lấy từ dữ liệu đính kèm trong thông báo
+  const openUrl = event.notification.data?.link;
+
+  event.waitUntil(
+      clients.matchAll({ type: 'window', includeUncontrolled: true }).then(clientList => {
+          // Kiểm tra nếu một tab với URL mở đã tồn tại
+          // for (let i = 0; i < clientList.length; i++) {
+          //     const client = clientList[i];
+          //     if (client.url === openUrl && 'focus' in client) {
+          //         return client.focus();
+          //     }
+          // }
+
+          // Nếu không có tab nào mở sẵn, tạo một tab mới
+          if (clients.openWindow) {
+              return clients.openWindow(openUrl);
+          }
+      })
+  );
+});
+
 // Give the service worker access to Firebase Messaging.
 // Note that you can only use Firebase Messaging here. Other Firebase libraries
 // are not available in the service worker.
@@ -23,15 +48,13 @@ messaging.onBackgroundMessage((payload) => {
   );
 
   // Customize notification here
-  const notification = otification.notification;
+  const notification = payload.notification;
   const notificationTitle = notification.title ?? '';
   const notificationOptions = {
     body: notification.body ?? '',
     icon: notification.icon ?? '',
-    data: notification.data ?? '',
-
+    data: payload.data ?? '',
   };
 
-  // self.registration.showNotification(notificationTitle, notificationOptions);
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
-
